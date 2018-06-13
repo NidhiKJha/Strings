dfd7468faef7b9213d4dee142d3c2c898efed9dc
Nidhi Kumari <nidhi1691996@outlook.com>
2018-06-14 01:59:30 +0530

    Add files via upload

---
+++ string_learning.py
@@ -0,0 +1,27 @@
+text = "hello dreams"
+p= text.count("e")
+print(p)
+q= text.lower()
+print(q)
+r=text.upper()
+print(r)
+s=text.capitalize()
+print(s)
+t = text.title()
+print(t)
+
+# to check text is lower
+print(text.islower)
+print(r.islower)
+
+a= "happy birthday"
+print(a.index("y"))
+
+print(a.find("day"))
+print(a.find("Day"))
+
+y = "0000birthday0000"
+p=y.strip("0")
+print(p)
+
+print(y.lstrip('0'))  #similarly rstrip for right striping
